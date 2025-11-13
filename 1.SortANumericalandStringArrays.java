8fa26ad2528d2b596a0b69b5d2e5947afd257db1
Neha <161303798+NehaKumari27102004@users.noreply.github.com>
2025-11-13 18:56:03 +0530

    Add files via upload

---
+++ 1.SortANumericalandStringArrays.java
@@ -0,0 +1,20 @@
+package Challenge;
+
+import java.util.Arrays;
+
+class SortANumericalandStringArrays {
+   /* public static void main(String[] args) {
+        int [] My_arr1={22,55,11,23,88,556,75,567,884,268,2,35,86};
+        String[] My_arr2={"Java","Python","Css","HTML","Javascript"};
+        System.out.println("Original array:" + Arrays.toString(My_arr1));
+        Arrays.sort(My_arr1);
+        System.out.println("The sorted numerical value is: " + Arrays.toString(My_arr1));
+
+        System.out.println("Original array:" + Arrays.toString(My_arr2));
+        Arrays.sort(My_arr2);
+        System.out.println("The sorted String value is:" + Arrays.toString(My_arr2));
+    }
+
+    */
+
+}
