6bf1418bd181785821fd83a168bf76ef4221f427
Neha <161303798+NehaKumari27102004@users.noreply.github.com>
2025-11-23 21:55:39 +0530

    Add files via upload

---
+++ 6.ReverseString.java
@@ -0,0 +1,17 @@
+package Challenge;
+
+class ReverseString {
+    public static void main(String[] args) {
+        String s = "Hii Neha , How are You?";
+        int len = s.length();
+        String rev = " ";
+
+        for(int i=len-1; i>=0; i--){
+            rev = rev + s.charAt(i);
+        }
+        System.out.println(rev);
+
+        StringBuffer sf=new StringBuffer(s);
+        System.out.println(sf.reverse());
+    }
+}
