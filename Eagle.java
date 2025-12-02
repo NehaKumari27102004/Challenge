e06931fc3fa96ecefe1425f7480d184d533b8d91
Neha <161303798+NehaKumari27102004@users.noreply.github.com>
2025-12-02 20:59:41 +0530

    Add files via upload

---
+++ Eagle.java
@@ -0,0 +1,12 @@
+package Challenge.ChallengeNo84;
+
+public class Eagle extends Bird{
+
+    public Eagle(){
+        super("Eagle");
+    }
+    @Override
+    public void fly() {
+        System.out.println("Eagle is Flying....");
+    }
+}
