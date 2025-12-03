0b08c599b613d935ea3b4b019987a1bb74bb134a
Neha <161303798+NehaKumari27102004@users.noreply.github.com>
2025-12-03 19:15:00 +0530

    Add files via upload

---
+++ TestTransportation.java
@@ -0,0 +1,29 @@
+package Polymorphism;
+
+public class TestTransportation {
+
+    public static void main(String[] args) {
+        Car c = new Car();
+       // Vehicle v = new Vehicle();
+        Plane p = new Plane();
+
+        //castTest(v);
+        castTest(c);
+        castTest(p);
+
+    }
+
+        //Vehicle vCar= new Car();
+        //Car cVehicle = (Car)new Vehicle();
+        private static void castTest(Vehicle veh){
+            veh.start();
+            //Car cVehicle = (Car) veh;
+            //((Car) veh).noOfDoors();
+            //cVehicle.start();
+            //cVehicle.noOfDoors();
+
+            if(veh instanceof  Car){
+
+            }
+        }
+}
