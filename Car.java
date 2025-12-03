f5bb8c2539259fe631c7c885440d740ef2a769c0
Neha <161303798+NehaKumari27102004@users.noreply.github.com>
2025-12-03 19:14:00 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,14 @@
+package Polymorphism;
+
+class Car implements Vehicle{
+
+    public int noOfDoors(){
+        return 5;
+    }
+
+    @Override
+    public void start() {
+        System.out.println("Car is starting");
+
+    }
+}
